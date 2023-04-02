26cc53f3919abfe900257a338adfb5bfba042ea1
Talha Shahid <m.talhas1993@gmail.com>
2023-04-03 02:03:31 +0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import SearchLocationInput from './SearchLocationInput';
+import SearchLocationInput from "./SearchLocationInput";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      <h2>Google Auto Search</h2>
       <SearchLocationInput onChange={() => null} />
     </div>
   );
--- src/SearchLocationInput.js
+++ src/SearchLocationInput.js
@@ -7,7 +7,7 @@ const loadScript = (url, callback) => {
   script.type = "text/javascript";
 
   if (script.readyState) {
-    script.onreadystatechange = function() {
+    script.onreadystatechange = function () {
       if (script.readyState === "loaded" || script.readyState === "complete") {
         script.onreadystatechange = null;
         callback();
@@ -45,7 +45,7 @@ function SearchLocationInput() {
 
   useEffect(() => {
     loadScript(
-      `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`,
+      `https://maps.googleapis.com/maps/api/js?key=AIzaSyCtABEXRCga9WoTi_pQ3d8lvvRJXlQ-T9o&libraries=places`,
       () => handleScriptLoad(setQuery, autoCompleteRef)
     );
   }, []);
@@ -54,7 +54,7 @@ function SearchLocationInput() {
     <div className="search-location-input">
       <input
         ref={autoCompleteRef}
-        onChange={event => setQuery(event.target.value)}
+        onChange={(event) => setQuery(event.target.value)}
         placeholder="Enter a City"
         value={query}
       />
